e9411cbe44e5a7cc08c5f3dde73cd982b3770d12
shajeed <shajeed@live.fr>
2025-03-21 23:27:16 -0400

    Fixed the Next Config File to ignore Animation error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-import { withSentryConfig } from '@sentry/nextjs';
+import { withSentryConfig } from "@sentry/nextjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -22,16 +22,11 @@ const sentryWebpackPluginOptions = {
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
-  // Automatically annotate React components to show their full name in breadcrumbs and session replay
-  reactComponentAnnotation: {
-    enabled: true,
-  },
+  // Disable React component annotation as it's causing issues with SSR
+  reactComponentAnnotation: false,
 
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/monitoring",
+  // Disable automatic instrumentation of server functions
+  autoInstrumentServerFunctions: false,
 
   // Hides source maps from generated client bundles
   hideSourceMaps: true,
